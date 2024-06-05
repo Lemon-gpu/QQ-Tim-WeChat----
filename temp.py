8a74199afa98a4a8505f734ba90a59f158cf0128
LemonPigeons <74308279+Lemon-gpu@users.noreply.github.com>
2024-06-05 15:31:08 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ import subprocess
 # 应用程序的路径，你需要根据实际情况来替换下面的路径
 qq_path = "C:\\Program Files\\Tencent\\QQNT\\QQ.exe"
 wechat_path = "C:\\Program Files\\Tencent\\WeChat\\WeChat.exe"
-tim_path = "C:\\Program Files (x86)\\Tencent\\TIM\\Bin\\QQScLauncher"
+# tim_path = "C:\\Program Files (x86)\\Tencent\\TIM\\Bin\\QQScLauncher"
 
 # 启动QQ
 subprocess.Popen(qq_path)
@@ -12,4 +12,4 @@ subprocess.Popen(qq_path)
 subprocess.Popen(wechat_path)
 
 # 启动TIM
-subprocess.Popen(tim_path)
+# subprocess.Popen(tim_path)
